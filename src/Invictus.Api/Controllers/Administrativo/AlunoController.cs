85838a515f0783db3f4245f80ab8e7e5ea6cf288
alvaroximenesrj <alvaroximenesrj@gmail.com>
2022-05-23 09:35:25 -0300

    commit

--- src/Invictus.Api/Controllers/Administrativo/AlunoController.cs
+++ src/Invictus.Api/Controllers/Administrativo/AlunoController.cs
@@ -5,7 +5,6 @@ using Invictus.QueryService.Utilitarios.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
